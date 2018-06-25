1b9773d89a60f07a4a66ff87f4b23db6785e8525
Craig Williams <Craig.Williams@lic.co.nz>
2018-06-25 14:50:18 +1200

    Replace common delimiting characters with spaces.

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 class RepositoryNameToOctopusProject {
     convert(repositoryName : string): string {
+        repositoryName = repositoryName.replace(".", " ");
+        repositoryName = repositoryName.replace("_", " ");
+        repositoryName = repositoryName.replace("-", " ");
         return repositoryName;
     }
 }
 
 let helper : RepositoryNameToOctopusProject = new RepositoryNameToOctopusProject();
-console.log(helper.convert("Minda Reports"));
+console.log(helper.convert("UIMinda.Reports_is-Cool"));
